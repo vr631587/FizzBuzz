e6c08c338331ec1e68463b1c8a5dbcb4ecd4d9da
Verender kumar <vr631587@dal.ca>
2020-02-20 14:25:04 -0400

    made the third elif statement

--- task0.py
+++ task0.py
@@ -1,8 +1,8 @@
 for i in range(1,100):
     if i//3==0:
         print("fizz")
-    elif i//5=0:
+    elif i//5==0:
         print("buzz")
-    elif i//15=0:
+    elif i//3==0 and i//5==0:
         print("FizzBuzz")
 
